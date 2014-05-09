10474f88d31a0f9666a3b872e49e0480610df46b
Nico <nico@nomail.jp>
2014-05-09 20:26:54 +0200

    Ajout de la gestion des musiques pour l'audio

--- src/audio/Audio.cpp
+++ src/audio/Audio.cpp
@@ -1,4 +1,14 @@
 
 #include "Audio.hpp"
 
+void Audio::setMusic(const std::string& file){
+	music.openFromFile(file);
+	music.play();
+	music.setLoop(true);
+}
+
+void Audio::setMusicVolume(float volume){
+	music.setVolume(volume);
+}
+
 
--- src/audio/Audio.hpp
+++ src/audio/Audio.hpp
@@ -17,8 +17,18 @@
 #ifndef H_AUDIO
 #define H_AUDIO
 
+#include <SFML/Audio.hpp>
+
 class Audio
 {
+	public:
+
+		void setMusic(const std::string& file);
+		void setMusicVolume(float volume);
+	
+	private:
+
+		sf::Music music;
 };
 
 #endif
