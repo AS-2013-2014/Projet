294d5b880d772de94f3f7c892504f8a9f6ea57d0
powaa308 <actimel-power@hotmail.fr>
2014-05-17 15:44:43 +0200

    Update Audio.hpp
    Ajout des constantes pour les sons et de paramètres comme Sound et SoundBuffer

--- src/audio/Audio.hpp
+++ src/audio/Audio.hpp
@@ -18,17 +18,29 @@
 #define H_AUDIO
 
 #include <SFML/Audio.hpp>
-
+    const int LOAD = 0;
+    const int COGNER_PLATEFORME = 1;
+    const int COGNER_OBSTACLE = 2;
+    const int TOMBE = 3;
+    const int CLIC = 4;
+    const int ASCENSEUR = 5;
+    const int SAUT = 6;
+    const int DOUBLE_SAUT = 7;
+    const int ATTERIE = 8;
+    const int PAUSE = 9;
+    const int PLATEFORME = 10;
 class Audio
 {
 	public:
-
 		void setMusic(const std::string& file);
+		void setSound(const int lequel);
 		void setMusicVolume(float volume);
-	
+		
 	private:
 
 		sf::Music music;
+		sf::Sound sound;
+        	sf::SoundBuffer sdBuff;
 };
 
 #endif
