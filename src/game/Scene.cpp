3229756dc9d52d2048a507d0fdfa43d568290426
ElyseC <elyse.cordeau@u-psud.fr>
2014-05-19 15:31:01 +0200

    Update Scene.cpp
    - on utilise des refs d'objets pour les collections
    - ajout des fonctions loadLevel et addPlateform
    - changement du constructeur

--- src/game/Scene.cpp
+++ src/game/Scene.cpp
@@ -1,60 +1,14 @@
 
 #include "Scene.hpp"
-#include "Game.hpp"
 
-Scene::Scene(Game *_game, const std::string &file): game(_game)
+
+Scene::Scene(Game *_game): game(_game)
 {
-    std::ifstream lvl_file(file.c_str(),std::ios::in);
+		
+	this->width = 0;
+	this->nb_sections = 0;
 
-		if(lvl_file)
-		{
-			//lecture de la longueur du niveau
-			std::string line;
-			getline(lvl_file,line);
-			this->width = readLvlWidth(line);
-
-			//création des sections
-			this->nb_sections = (int)ceil(width/SECTION_WIDTH);
-
-			for (int i=0 ; i<nb_sections ; i++)
-			{
-					sections.push_back(Section());
-			}
-			std::cout<<nb_sections<<std::endl;
-
-			//création des plateformes
-			getline(lvl_file,line);
-			Platform p;
-			while (line != "END")
-			{
-					p = readPlateform(line);
-					platforms.push_back(p);
-
-					//on ajoute la plateforme aux sections correspondantes
-					int section_min = (int)p.getX()/SECTION_WIDTH;
-					int section_max = (int)p.getX()/SECTION_WIDTH;
-					for (int i=0; i<3; i++)
-					{
-							if ((int)p.getOtherX()[i]/SECTION_WIDTH < section_min)
-									section_min = p.getOtherX()[i];
-							else if (p.getOtherX()[i]/SECTION_WIDTH > section_min)
-									section_max = p.getOtherX()[i];
-					}
-					for (int i=section_min; i<=section_max; i++)
-					{
-							sections[i].platforms.push_back(p);
-					}
-
-					//le mettre dans les autres sections ^^
-
-					getline(lvl_file,line);
-			}
-
-			//fermer le fichier
-			lvl_file.close();
-		}
-
-  //chargement du shader
+	//chargement du shader
 	sh_fade = new sf::Shader;
 	if(sf::Shader::isAvailable() && sh_fade->loadFromFile("misc/fade.frag", sf::Shader::Fragment))
     sh_fade->setParameter("texture", sf::Shader::CurrentTexture);
@@ -64,11 +18,92 @@ Scene::~Scene()
 {
 	for(int i = 0; i < graphics.size(); i++)
 		delete graphics[i];
+	
+	for(int i = 0; i < entities.size(); i++)
+		delete entities[i];
+
+	for(int i = 0; i < sections.size(); i++)
+		delete sections[i];
 
 	delete sh_fade;
 }
 
-Platform readPlateform(std::string line)
+void Scene::loadLevel(const std::string &file)
+{
+	std::ifstream lvl_file(file.c_str(),std::ios::in);
+	
+	if(lvl_file)
+	{
+		//lecture de la longueur du niveau
+		std::string line;
+		getline(lvl_file,line);
+		this->width = readLvlWidth(line);
+
+		//création des sections pour la gestion des collisions
+		this->nb_sections = (int)ceil(width/SECTION_WIDTH);
+
+		for (int i=0 ; i<nb_sections ; i++)
+		{
+				sections.push_back(new Section());
+		}
+
+		//création des plateformes
+		getline(lvl_file,line);
+		Platform* p;
+		while (line != "END")
+		{				
+				p = readPlateform(line);
+
+				//on ajoute la plateforme aux entitées du niveau
+				entities.push_back(p);
+
+				//on ajoute la plateforme aux sections correspondantes
+				int section_min = (int)p->getX()/SECTION_WIDTH;
+				int section_max = (int)p->getX()/SECTION_WIDTH;
+				for (int i=0; i<3; i++)
+				{
+						if ((int)p->getOtherX()[i]/SECTION_WIDTH < section_min)
+								section_min = p->getOtherX()[i];
+
+						else if (p->getOtherX()[i]/SECTION_WIDTH > section_min)
+								section_max = p->getOtherX()[i];
+				}
+				for (int i=section_min; i<=section_max; i++)
+				{
+						sections[i]->platforms.push_back(p);
+				}
+
+				getline(lvl_file,line);
+		}
+
+		//fermeture du fichier
+		lvl_file.close();
+	}
+}
+
+void Scene::addPlateform(Plateform* p) 
+{
+	//on ajoute la plateforme aux entitées du niveau
+	entities.push_back(p);
+
+	//on ajoute la plateforme aux sections correspondantes
+	int section_min = (int)p->getX()/SECTION_WIDTH;
+	int section_max = (int)p->getX()/SECTION_WIDTH;
+	for (int i=0; i<3; i++)
+	{
+			if ((int)p->getOtherX()[i]/SECTION_WIDTH < section_min)
+					section_min = p->getOtherX()[i];
+
+			else if (p->getOtherX()[i]/SECTION_WIDTH > section_min)
+					section_max = p->getOtherX()[i];
+	}
+	for (int i=section_min; i<=section_max; i++)
+	{
+			sections[i]->platforms.push_back(p);
+	}
+}
+
+Platform* readPlateform(std::string line)
 {
 	int i = 0;
 
@@ -141,7 +176,7 @@ Platform readPlateform(std::string line)
     }
     int skin = atoi(tmp.c_str());
 
-    return Platform(x,y,z,length,width,angle,type,skin);
+    return Platform* = new Platform(x,y,z,length,width,angle,type,skin);
 }
 
 int readLvlWidth(std::string line)
