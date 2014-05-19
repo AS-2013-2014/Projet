10ef1b4d766d6a8fc37aa7046230bdad468b6dd9
ElyseC <elyse.cordeau@u-psud.fr>
2014-05-19 15:29:38 +0200

    Update Scene.hpp
    Maintenant on gère des collection de ref d'objets :)

--- src/game/Scene.hpp
+++ src/game/Scene.hpp
@@ -18,7 +18,9 @@
 #define SCENE
 
 #include "Section.hpp"
+#include "Game.hpp"
 #include "entities/Platform.hpp"
+#include "entities/Entity.hpp"
 #include "entities/WImage.hpp"
 #include "entities/WAnimation.hpp"
 #include <math.h>
@@ -38,33 +40,33 @@ class Game;
 class Scene : public sf::Drawable, public sf::Transformable
 {
 	public:
-	std::vector<Section> sections;
-	std::vector<Platform> platforms;
-	int width;
-	int nb_sections;
+	std::vector<Section*> sections;
+	std::vector<Entity*> entities;
 
 	Scene(Game *game,const std::string &file);
 	~Scene();
 
 	bool loadGraphics(const std::string& file);
-  void setBackground(const std::string file);
+	void setBackground(const std::string file);
 	void frame(float time);
+	void loadLevel(const std::string &file); // charge le fichier txt du level
 
 	//accesseurs
 	sf::Vector2f& getCam(){ return cam; }
 
 	private:
-
+	int width;
+	int nb_sections;
 	virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const;
 	std::vector<Graphic*> graphics;
 	sf::Vector2f cam;
 
 	Game *game;
-  sf::Sprite background;
-  sf::Shader* sh_fade;
+    sf::Sprite background;
+    sf::Shader* sh_fade;
 };
 
-Platform readPlateform(std::string line);
+Platform* readPlateform(std::string line);
 int readLvlWidth(std::string line);
 
 #endif
