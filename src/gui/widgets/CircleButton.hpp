4063b8468e422f3823f0c98f3031ae28ab493cc1
powaa308 <actimel-power@hotmail.fr>
2014-06-17 15:51:54 +0200

    Create CircleButton.hpp

---
+++ src/gui/widgets/CircleButton.hpp
@@ -0,0 +1,30 @@
+/****************************************************************************/
+/*                       DUT INFO AS - Projet AS                            */
+/*                                                                          */
+/*                                                                          */
+/* Categorie: interface                                                     */
+/*                                                                          */
+/* Fonction(s):                                                             */
+/*--------------------------------------------------------------------------*/
+/* Description: Un bouton                                                   */
+/*                                                                          */
+/*                                                                          */
+/*                                                                          */
+/*                                                                          */
+/****************************************************************************/
+
+#ifndef CIRCLE_BUTTON
+#define CIRCLE_BUTTON
+
+#include "../Widget.hpp"
+
+class CircleButton : public Widget
+{
+public:
+  CircleButton()
+  {
+
+  }
+};
+
+#endif
