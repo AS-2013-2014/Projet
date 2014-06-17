518a42a5eadbb143a611d41dd74d3c165c0b6c01
powaa308 <actimel-power@hotmail.fr>
2014-06-17 15:51:17 +0200

    Update Page.hpp

--- src/gui/Page.hpp
+++ src/gui/Page.hpp
@@ -1,4 +1,3 @@
-
 /****************************************************************************/
 /*                       DUT INFO AS - Projet AS                            */
 /*                                                                          */
@@ -16,9 +15,64 @@
 
 #ifndef H_PAGE
 #define H_PAGE
+#include <SFML/Graphics.hpp>
+#include <vector>
+#include "Widget.hpp"
+#include "widgets/RectangleButton.hpp"
+#include "widgets/CircleButton.hpp"
+#include	"widgets/Slider.hpp"
+using namespace sf;
+using namespace std;
 
 class Page
 {
+private:
+  // Attributs
+
+
+public:
+  vector<RectangleButton>	mesRect;
+  vector<CircleButton>	mesCircl;
+  vector<Slider>	mesSlid;
+  string cheminImage;
+
+  // Constructeurs
+  Page ()
+  {
+
+  }
+
+  Page (vector<RectangleButton> widgets, string chemin)
+  {
+    mesRect = widgets;
+    cheminImage = chemin;
+  }
+
+  ~Page()
+  {
+    /*for(int i=0; i<mesWidgets.size(); ++i)
+    {
+        delete mesWidgets[i];  //On libère la i-ème case mémoire allouée
+        mesWidgets[i] = 0;  //On met le pointeur à 0 pour éviter les soucis
+    }*/
+  }
+
+  // Méthodes
+  void ajouterWidget (RectangleButton monWidget)
+  {
+    mesRect.push_back(monWidget);
+  }
+
+  void ajouterWidget (CircleButton monWidget)
+  {
+    mesCircl.push_back(monWidget);
+  }
+
+  void ajouterWidget (Slider monWidget)
+  {
+    mesSlid.push_back(monWidget);
+  }
+
 };
 
 #endif
