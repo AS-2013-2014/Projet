0dcb12f1db5bcfffa56b47b58b1ab54ea6212ed3
powaa308 <actimel-power@hotmail.fr>
2014-06-18 11:39:33 +0200

    Update RectangleButton.cpp
    Inversion de largeur et longueur dans la fonction estDansButton()

--- src/gui/widgets/RectangleButton.cpp
+++ src/gui/widgets/RectangleButton.cpp
@@ -1,58 +1,91 @@
 #include <SFML/Graphics.hpp>
-#include "Button.h"
+#include "RectangleButton.hpp"
 #include <iostream>
 using namespace std;
 
-// methode permettant l'affichage à l'ecran
+
+RectangleButton::RectangleButton(){}
+RectangleButton::RectangleButton(int _x, int _y, int _width, int _height, sf::String textB)
+{
+
+  x_=_x;
+  y_=_y;
+  longueur_=_height;
+  largeur_=_width;
+
+  if (!font.loadFromFile("resources/sansation.ttf"))
+    {
+      cout << "probleme de chargement" << endl;
+    }
+// Definir la police du texte ainsi que sa taille
+
+  text.setFont(font);
+  text.setString(textB);
+  text.setCharacterSize(20);
+
+// Recuperation des informations du texte pour le centrer dans le bouton
+  float tt = text.getLocalBounds().width;
+  float uu = text.getLocalBounds().height;
+
+  text.setPosition(sf::Vector2f( (_width/2 - tt/2) , (_height-uu)/ 2 - (text.getCharacterSize()-uu) ));
+  text.setColor(sf::Color::Black);
+
+
+// On defini la taille ainsi que le position du bouton
+  rect.setSize(sf::Vector2f(_width, _height));
+  setPosition(sf::Vector2f(_x,_y));
+
+  rect.setFillColor(sf::Color::White);
+
+}
 void RectangleButton::draw(sf::RenderTarget& target, sf::RenderStates states) const
 {
-    // on applique la transformation de l'entité -- on la combine avec celle qui a été passée par l'appelant
-    states.transform *= getTransform(); // getTransform() est définie par sf::Transformable
+  // on applique la transformation de l'entitÃ© -- on la combine avec celle qui a Ã©tÃ© passÃ©e par l'appelant
+  states.transform *= getTransform(); // getTransform() est dÃ©finie par sf::Transformable
 
-    // on applique la texture
-    states.texture = NULL;
-    target.draw(rect, states);
-    target.draw(text, states);
+  // on applique la texture
+  states.texture = NULL;
+  target.draw(rect, states);
+  target.draw(text, states);
 }
- void RectangleButton::setColorButton(sf::Color color){
-        rect.setFillColor(color);
-    }
-/*
-int main()
+void RectangleButton::setColorButton(sf::Color color)
 {
-
-    RectangleButton b1( 150, 80, 400, 50,"TEST BUTTON");
-
-    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML works!");
-
-    // sf::Color color = sf::Color(190,190,190,255);
-
-
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                {window.close();}
-
-                    if(event.mouseButton.x > 150 && event.mouseButton.x < 550 && event.mouseButton.y > 80 && event.mouseButton.y < 130)
-                    {
-                        cout<<"Blue"<<endl;
-                        cout<<event.mouseMove.x << " - " << event.mouseMove.y <<endl;
-                        b1.setColorBushape.setOutlineThickness(10);
-shape.setOutlineColor(sf::Color(250, 150, 100));tton(sf::Color(105,105,105,20));
-
-                    }
-                    else{  b1.setColorButton(sf::Color::White);}
-
-        }
-        sf::Texture texture;
-        window.clear(sf::Color::White);
-        window.draw(b1);
-        window.display();
-    }
-
-
+  rect.setFillColor(color);
 }
-*/
+
+
+/** Getters **/
+int RectangleButton::getX() const
+{
+  return x_;
+}
+int RectangleButton::getY() const
+{
+  return y_;
+}
+int RectangleButton::getLongueur() const
+{
+  return longueur_;
+}
+int RectangleButton::getLargeur() const
+{
+  return largeur_;
+}
+
+sf::Text  RectangleButton::getTexte(){
+  return  text;
+}
+
+/** Fonction qui dÃ©termine si le clique a eu lieu dans le Button **/
+bool RectangleButton::estDansButton(int x,int y) const
+{
+  //sf::Event event;
+  if(x > x_ && x < (x_ + largeur_)
+      && y > y_ && y < (y_ + longueur_)
+    )
+    return true;
+  else
+    return false;
+}
+/** Si bouton rectangulaire marche, utiliser les formes sfml (rectangle, cercle...) **/
+
