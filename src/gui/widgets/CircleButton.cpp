00998bbc89ec237eadd1ee1d67a2c52412a1b30e
mohamed <braham_mohamed@ymail.com>
2014-06-18 10:26:16 +0200

    Update CircleButton & RectangleButton
    Voici les code sources des deux type de boutons.
    les commentaire ne sont pas fait, il vont être fait dans l'heure

--- src/gui/widgets/CircleButton.cpp
+++ src/gui/widgets/CircleButton.cpp
@@ -1,3 +1,47 @@
-
+#include <SFML/Graphics.hpp>
 #include "CircleButton.hpp"
+#include <iostream>
+using namespace std;
 
+
+void CircleButton::draw(sf::RenderTarget& target, sf::RenderStates states) const
+{
+    // on applique la transformation de l'entité -- on la combine avec celle qui a été passée par l'appelant
+    states.transform *= getTransform(); // getTransform() est définie par sf::Transformable
+
+    // on applique la texture
+    states.texture = NULL;
+    target.draw(circle, states);
+}
+//on defini la courleur du cercle ainsi que la couleur de son anneau
+ void CircleButton::setColorButton(sf::Color colorCircle, sf::Color colorRing){
+        circle.setFillColor(colorCircle);
+        circle.setOutlineColor(colorRing);
+    }
+
+
+
+int main()
+{
+
+    CircleButton b1( 300, 300, 120 ,sf::Color::Blue, sf::Color::Cyan);
+
+    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML works!");
+
+
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                {window.close();}
+        }
+
+        window.draw(b1);
+        window.display();
+    }
+
+
+}
--- src/gui/widgets/CircleButton.hpp
+++ src/gui/widgets/CircleButton.hpp
@@ -1,30 +1,60 @@
-/****************************************************************************/
-/*                       DUT INFO AS - Projet AS                            */
-/*                                                                          */
-/*                                                                          */
-/* Categorie: interface                                                     */
-/*                                                                          */
-/* Fonction(s):                                                             */
-/*--------------------------------------------------------------------------*/
-/* Description: Un bouton                                                   */
-/*                                                                          */
-/*                                                                          */
-/*                                                                          */
-/*                                                                          */
-/****************************************************************************/
+/************************************************************************/
+/*                      DUT INFO AS - PROJET AS                         */
+/*                                                                      */
+/*                                                                      */
+/* Categorie: Equipe Interface Utilisateur (GUI)                        */
+/*                                                                      */
+/* Fonction : Classe CircleButton                                       */
+/* ------------------------------------------------------------         */
+/* Description :                                                        */
+/*  Cette classe permet de créer un bouton circulaire avec un anneau de */
+/*   couleur differentes.                                               */
+/*                                                                      */
+/*                                                                      */
+/************************************************************************/
+#ifndef BUTTON
+#define BUTTON
+#include <SFML/Graphics.hpp>
 
-#ifndef CIRCLE_BUTTON
-#define CIRCLE_BUTTON
+/*  Classe : CircleButton
+/*  Parametre du contructeur : - Sa position ( x, y ), son Rayon
+/*
+/*
+/*
+/*
+/*
+/**/
 
-#include "../Widget.hpp"
 
-class CircleButton : public Widget
-{
-public:
-  CircleButton()
-  {
+class CircleButton : public sf::Drawable, public sf::Transformable {
 
-  }
+// definition des attributs afin de concevoir le bouton
+    sf::CircleShape circle;
+    sf::Texture texture;
+
+public :
+    CircleButton(int _x, int _y, int _dimension, sf::Color colorCircle, sf::Color colorRing)
+    {
+
+// On defini la taille , la couleur ainsi que le position du bouton
+        circle.setRadius(_dimension);
+        setPosition(sf::Vector2f(_x,_y));
+        circle.setFillColor(colorCircle);
+
+// on defini la largeur de l'anneau
+        circle.setOutlineThickness(0.3*_dimension);
+        circle.setOutlineColor(colorRing);
+
+    }
+
+// procédure qui permet de redéfinir les couleur du circleButton
+    void setColorButton(sf::Color colorCircle, sf::Color colorRing);
+
+// redifnition de la méthode draw afin de dessiné le circleButton
+virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const;
+
+
 };
+
 
 #endif
